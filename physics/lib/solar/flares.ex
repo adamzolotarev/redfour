56e8cda8322a7f5b0a419fe767185b0078ade5e2
Adam Zolotarev <eugenezolo@gmail.com>
2016-05-11 19:05:32 -0400

    Added several versions of Load

--- physics/lib/solar/flares.ex
+++ physics/lib/solar/flares.ex
@@ -1,8 +1,49 @@
-defmodule Solar do
+defmodule Solar.Flare do
+
+  defstruct [
+    classification: :M,
+    scale: 0,
+    power: 0,
+    is_deadly: false,
+    date: nil
+  ]
+
+  # updated_map = %{map | key: "val"}
+  # updated_map = Map.put(map, :key, "value")
+  # updated_map = Map.put_new(map, :new_key, "value")
+
+  def load(flares) do
+    Enum.map flares, fn(flare) ->
+      flare
+        |> power
+        |> calculate_deadliest
+    end
+  end
+
+  def load_with_comprehensions(flares) do
+      for flare <- flares,
+        power <- [power(flare)],
+        is_deadly <- [power > 1000],
+        do: %{flare | power: power, is_deadly: is_deadly}
+  end
+
+  def load_with_comprehensions_pipe(flares) do
+    for flare <- flares, do: flare |> calculate_power |> calculate_deadliest
+  end
+
 
   def power(%{classification: :X, scale: s}), do: 1000 *s
   def power(%{classification: :M, scale: s}), do: 10 *s
   def power(%{classification: :C, scale: s}), do: s
+
+  def calculate_power(flare) do
+    factor = case flare.classification do
+      :M -> 10
+      :X -> 1000
+      :C -> 1
+    end
+    %{flare | power: flare.scale * factor}
+  end
 
   def no_eva(flares) do
     Enum.filter flares, &(power(&1) > 1000)
@@ -14,6 +55,11 @@ defmodule Solar do
     Enum.map(flares, &(power(&1)))
       |> Enum.max
   end
+
+  def calculate_deadliest(flare) do
+    %{flare | is_deadly: flare.power > 1000 }
+  end
+
 
   def total_flare_power(flares) do
     Enum.map(flares, &(power(&1)))
--- physics/test/solar_test.exs
+++ physics/test/solar_test.exs
@@ -1,19 +1,20 @@
 defmodule SolarTest do
   use ExUnit.Case
   use Timex
+  import Solar.Flare
   # use Physics.Rocketry
-  doctest Solar
+  doctest Solar.Flare
 
   setup do
     flares =[
-      %{classification: :X, scale: 99, date: Date.from({1859,8,29})},
-      %{classification: :M, scale: 5.8, date: Date.from({2015, 1, 12})},
-      %{classification: :M, scale: 1.2, date: Date.from({2015, 2, 9})},
-      %{classification: :C, scale: 3.2, date: Date.from({2015, 4, 18})},
-      %{classification: :M, scale: 83.6, date: Date.from({2015, 6, 23})},
-      %{classification: :C, scale: 2.5, date: Date.from({2015, 7, 4})},
-      %{classification: :X, scale: 72, date: Date.from({2012, 7, 23})},
-      %{classification: :X, scale: 45, date: Date.from({2003, 11, 4})},
+      %Solar.Flare{classification: :X, scale: 99, date: Date.from({1859,8,29})},
+      %Solar.Flare{classification: :M, scale: 5.8, date: Date.from({2015, 1, 12})},
+      %Solar.Flare{classification: :M, scale: 1.2, date: Date.from({2015, 2, 9})},
+      %Solar.Flare{classification: :C, scale: 3.2, date: Date.from({2015, 4, 18})},
+      %Solar.Flare{classification: :M, scale: 83.6, date: Date.from({2015, 6, 23})},
+      %Solar.Flare{classification: :C, scale: 2.5, date: Date.from({2015, 7, 4})},
+      %Solar.Flare{classification: :X, scale: 72, date: Date.from({2012, 7, 23})},
+      %Solar.Flare{classification: :X, scale: 45, date: Date.from({2003, 11, 4})},
     ]
     {:ok, data: flares}
   end
@@ -23,47 +24,47 @@ defmodule SolarTest do
   end
 
   test "power of X classification with 99 scale is 99000" do
-    p = Solar.power(%{classification: :X, scale: 99})
+    p = power(%{classification: :X, scale: 99})
     assert p == 99000
   end
 
   test "power of M classification with 99 scale is 990" do
-    p = Solar.power(%{classification: :M, scale: 99})
+    p = power(%{classification: :M, scale: 99})
     assert p == 990
   end
 
   test "power of c classification with 99 scale is 99" do
-    p = Solar.power(%{classification: :C, scale: 99})
+    p = power(%{classification: :C, scale: 99})
     assert p == 99
   end
 
   test "No EVA when power is greater than 1000" do
-    p = Solar.no_eva([%{classification: :X, scale: 2}])
+    p = no_eva([%{classification: :X, scale: 2}])
     assert length(p) == 1
   end
 
   test "Do EVA when power is 1000" do
-    p = Solar.no_eva([%{classification: :X, scale: 1}])
+    p = no_eva([%{classification: :X, scale: 1}])
     assert length(p) == 0
   end
 
   test "Do EVA when power is less than 1000" do
-    p = Solar.no_eva([%{classification: :M, scale: 99}])
+    p = no_eva([%{classification: :M, scale: 99}])
     assert length(p) == 0
   end
 
   test "Go inside", %{data: flares} do
-    d = Solar.no_eva(flares)
+    d = no_eva(flares)
     assert length(d) == 3
   end
 
   test "Deadliest flare is 99000", %{data: flares} do
-    d = Solar.deadliest(flares)
+    d = deadliest(flares)
     assert d == 99000
   end
 
   test "A list of flares", %{data: flares} do
-    result = Solar.flare_list(flares)
+    result = flare_list(flares)
     assert result == [
     %{power: 99000, is_deadly: true},
     %{power: 58.0, is_deadly: false},
@@ -74,6 +75,10 @@ defmodule SolarTest do
     %{power: 72000, is_deadly: true},
     %{power: 45000, is_deadly: true}
   ]
+  end
+
+  test "A Solar Flare is a Map with a special key" do
+    assert %Solar.Flare{}.__struct__ == Solar.Flare
   end
 
 end
